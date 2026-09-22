eefaaf61689b663743d8b0f4f37582e93e4a87b7
BenedekCodes <benedek.toth@paconsulting.com>
2026-09-22 13:21:57 +0100

    Added graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,15 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ status: "active", message: "CoSpace API is running" });
 });
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
 
-process.on("SIGTERM", () => process.exit(0));
-process.on("SIGINT", () => process.exit(0));
+const shutdown = () => {
+  server.close(() => process.exit(0));
+};
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
 
 export default app;
